33af313cbccf3b1c2d9fba46780e843a7d37741e
Shiryee Lin <Cat@xyl.icu>
2021-01-02 15:01:56 +0800

    Modify Gateway IP

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 ![OPhub logo](/logo.svg)
-
--- router/oolite_v5.2-dev(WIFI)/lean.sh
+++ router/oolite_v5.2-dev(WIFI)/lean.sh
@@ -6,5 +6,5 @@ rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 popd
 
-sed -i 's/192.168/10.168/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
